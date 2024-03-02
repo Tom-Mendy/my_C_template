9230baa0d66f0118c719313c199b9f45b44b280d
Tom Mendy <tom.mendy@epitech.eu>
2024-03-02 14:29:01 +0100

    [FIX] test my_show_word_array.c

--- lib/my_str/tests/test_my_show_word_array.c
+++ lib/my_str/tests/test_my_show_word_array.c
@@ -5,29 +5,28 @@
 ** test_my_show_word_array
 */
 
+#include "my_str.h"
 #include <criterion/criterion.h>
 #include <criterion/redirect.h>
-#include "my_str.h"
 
-void redirect_my_show_word_array(void)
-{
-    cr_redirect_stdout();
-    cr_redirect_stderr();
+void redirect_my_show_word_array(void) {
+  cr_redirect_stdout();
+  cr_redirect_stderr();
 }
 
 Test(my_show_word_array, basic_test, .init = redirect_my_show_word_array) {
-    char *tab[3] = {"poule", "TOTO", "tata"};
-    my_show_word_array(tab);
-    cr_assert_stdout_eq_str("poule\nTOTO\ntata\n");
+  char *tab[4] = {"poule", "TOTO", "tata", NULL};
+  my_show_word_array(tab);
+  cr_assert_stdout_eq_str("poule\nTOTO\ntata\n");
 }
 
 Test(my_show_word_array, rigor, .init = redirect_my_show_word_array) {
-    char *tab[3] = {"TOTO", NULL, "tata"};
-    my_show_word_array(tab);
-    cr_assert_stdout_eq_str("TOTO\n");
+  char *tab[4] = {"TOTO", NULL, "tata", NULL};
+  my_show_word_array(tab);
+  cr_assert_stdout_eq_str("TOTO\n");
 }
 
 Test(my_show_word_array, rigor_output) {
-    char *tab[3] = {NULL, "TOTO", "tata"};
-    cr_assert_eq(my_show_word_array(tab), 0);
+  char *tab[4] = {NULL, "TOTO", "tata", NULL};
+  cr_assert_eq(my_show_word_array(tab), 0);
 }
